d14d0ce30e423ac6a4c05c073f3167ac11f00795
Alex Faber <alexfaber2011@gmail.com>
2014-01-18 23:56:49 -0600

    matches nearly done

--- templates/matches.php
+++ templates/matches.php
@@ -16,17 +16,85 @@
 							$cursor = $db->Lost->find($query);
 							
 							foreach($cursor as $doc){
-								if(isset($doc['PMatch_id'])){
-									foreach($doc['PMatch_id'] as $found_id){
-										echo $doc['Item'] . "   " . $found_id . "<br /><br />";
-									}
+								if(isset($doc['PMatch_id'])){?>
+									<?php foreach($doc['PMatch_id'] as $found_id): ?>
+										<?php
+											$query = array("_id" => new MongoId($found_id));
+											$their_document = $db->Found->findOne($query);
+										?>
+										<div class="match-container">
+												<div class="arrow"> <!--  --></div>
+												<div class="your-item-container">
+													<h1><?php echo $doc['Item']?></h1>
+													<table>
+														<tr>
+															<td>Reported Lost:</td>
+															<td><?php echo $doc['Date Created'] ?></td>
+														</tr>
+														<tr>
+															<td>Location:</td>
+															<td><?php echo $doc['Location'] ?></td>
+														</tr>
+														<tr>
+															<td class="description" colspan="2"><?php echo $doc['Description'] ?></td>
+														</tr>
+														<tr>
+															<td class="tags" colspan="2">
+																<?php 
+																	foreach($doc["Tags"] as $tag){
+																		echo $tag . ", ";
+																	}
+																?>
+															</td>
+														</tr>
+													</table>
+												</div>
+												<div class="their-item-container">
+													<form id="reject" action="http://mysterious-stream-6921.herokuapp.com/" methed="post">
+														<input type="hidden" name="Reject" value="<?php echo $found_id ?>" />
+														<input type="hidden" name="Lost" value="<?php echo $doc["_id"] ?>" />
+														<input type="submit" id="reject" value="NO" />
+													</form>
+													<form id="accept" action="http://mysterious-stream-6921.herokuapp.com/" methed="post">
+														<input type="hidden" name="Accept" value="<?php echo $found_id ?>" />
+														<input type="hidden" name="Lost" value="<?php echo $doc["_id"] ?>" />
+														<input type="submit" id="accept" value="YES" />
+													</form>
+													<h1><?php echo $their_document['Item'] ?></h1>
+													<table>
+														<tr>
+															<td>Reported Lost:</td>
+															<td><?php echo $their_document['Date Created'] ?></td>
+														</tr>
+														<tr>
+															<td>Location:</td>
+															<td><?php echo $their_document['Location'] ?></td>
+														</tr>
+														<tr>
+															<td class="description" colspan="2"><?php echo $their_document['Description'] ?></td>
+														</tr>
+														<tr>
+															<td class="tags" colspan="2">
+																<?php 
+																	foreach($their_document["Tags"] as $tag){
+																		echo $tag . ", ";
+																	}
+																?>
+															</td>
+														</tr>
+														<tr>
+															<td>Email: </td>
+															<td><?php echo $their_document["email"] ?></td>
+														</tr>
+													</table>
+												</div>
+											</div>
+									<?php endforeach ?>
+								<?php
 								}
 							}
 						}
 					?>
-					<div class="match-container">
-						
-					</div>
 				</div>
 			</div>
 		</div>
